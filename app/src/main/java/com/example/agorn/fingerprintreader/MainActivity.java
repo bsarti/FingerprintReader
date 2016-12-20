7fe86f8f10a6f63102a4948ece1fcb758300d6ae
bsarti <benjamin.sarti@gmail.com>
2016-12-20 14:37:09 +0530

    first commit

---
+++ app/src/main/java/com/example/agorn/fingerprintreader/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.agorn.fingerprintreader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
